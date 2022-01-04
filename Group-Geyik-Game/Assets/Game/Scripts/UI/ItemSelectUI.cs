73634a900e399e1ef7cdf7d3296cbc89bcc0976f
OmerAsik <faruk.asikoglu@gmail.com>
2022-01-04 04:26:13 +0300

    refactor: ItemSelectUI
    Get random items with random order for limited UI slot

--- Group-Geyik-Game/Assets/Game/Scripts/UI/ItemSelectUI.cs
+++ Group-Geyik-Game/Assets/Game/Scripts/UI/ItemSelectUI.cs
@@ -4,48 +4,75 @@ using UnityEngine;
 using UnityEngine.UI;
 using NaughtyAttributes;
 
-
 public class ItemSelectUI : MonoBehaviour
 {
-    [SerializeField] private List<Transform> itemList;
+    [SerializeField] private List<Transform> itemList; // UI'daki 3 slot
 
-    private HairTypeListSO hairTypeList;
+    private HairTypeListSO hairList;
     private EyeListSO eyeList;
 
+    private const int maxItemCount = 3;
 
     private void Awake()
     {
         //datalarýmýz
-        hairTypeList = Resources.Load<HairTypeListSO>(typeof(HairTypeListSO).Name);
+        hairList = Resources.Load<HairTypeListSO>(typeof(HairTypeListSO).Name);
         eyeList = Resources.Load<EyeListSO>(typeof(EyeListSO).Name);
 
-        GetHairs();
-
+        //GetHairs();
     }
 
     [Button]
     private void GetHairs()
     {
-        int index = 0;
+        List<int> tempList = new List<int>();
         foreach (Transform transform in itemList)
         {
             Image image = transform.Find(StringData.IMAGE).GetComponent<Image>();
-            image.sprite = hairTypeList.list[index].sprite;
-            image.color = UtilsClass.GetColorFromString(hairTypeList.list[index].colorHex);
-            index++;
+
+            bool isAllItemSlotsFilled = maxItemCount == tempList.Count;
+
+            // Farklý saçlarýn farklý sýrada olacak þekilde UI'a dizilmesi. 
+            //3'ten fazla saç varsa sadece 3 tanesinin alýnmasý. Çýldýrdým yazarken
+            while (!isAllItemSlotsFilled)
+            {
+                int randomIndex = UnityEngine.Random.Range(0, hairList.list.Count);
+
+                if (!tempList.Contains(randomIndex))
+                {
+                    image.sprite = hairList.list[randomIndex].sprite;
+                    image.color = UtilsClass.GetColorFromString(hairList.list[randomIndex].colorHex);
+                    tempList.Add(randomIndex);
+                    isAllItemSlotsFilled = true;
+                }
+            }
         }
+        tempList.Clear();
     }
 
     [Button]
     private void GetEyes()
     {
-        int index = 0;
+        List<int> tempList = new List<int>();
         foreach (Transform transform in itemList)
         {
             Image image = transform.Find(StringData.IMAGE).GetComponent<Image>();
-            image.sprite = eyeList.list[index].sprite;
-            image.color = UtilsClass.GetColorFromString(eyeList.list[index].colorHex);
-            index++;
+
+            bool isAllItemSlotsFilled = maxItemCount == tempList.Count;
+
+            while (!isAllItemSlotsFilled)
+            {
+                int randomIndex = UnityEngine.Random.Range(0, eyeList.list.Count);
+
+                if (!tempList.Contains(randomIndex))
+                {
+                    image.sprite = eyeList.list[randomIndex].sprite;
+                    image.color = UtilsClass.GetColorFromString(eyeList.list[randomIndex].colorHex);
+                    tempList.Add(randomIndex);
+                    isAllItemSlotsFilled = true;
+                }
+            }
         }
+        tempList.Clear();
     }
 }
