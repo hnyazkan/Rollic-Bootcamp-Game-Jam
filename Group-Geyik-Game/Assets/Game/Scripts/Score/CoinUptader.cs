b1d10b92ea5fdb6f36706b31ef1db80a29d9b9ae
Sena Celebi <43573954+SenaCelebi@users.noreply.github.com>
2022-01-07 17:17:30 +0300

    update: coinuptader

--- Group-Geyik-Game/Assets/Game/Scripts/Score/CoinUptader.cs
+++ Group-Geyik-Game/Assets/Game/Scripts/Score/CoinUptader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class WinUIController : MonoBehaviour
+public class CoinUptader : MonoBehaviour
 {
     public TextMeshProUGUI coinsText;
  
--- Group-Geyik-Game/Assets/Game/Scripts/Score/Score.cs
+++ Group-Geyik-Game/Assets/Game/Scripts/Score/Score.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
+    public static Score Instance { get; private set; }
+    
     public static int stars = 3;
     public static int totalCoin = 0;
 
+    private void Awake()
+    {
+        Instance = this;
+    } 
+
+  
     /*
      * Nerede player yanlýþ seçim yaparsa star sayýsýný azalt.
      * Score.stars--
